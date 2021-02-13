770483e221427431ecbb42c169fa939c5b1d65ef
Andrey <andrkijonok@gmail.com>
2021-02-13 12:46:44 +0200

    Crops and food added to game

---
+++ Assets/Scripts/Crop.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+    private int STEP_EMPTY = 0;
+    private int STEP_GROWS = 1;
+    private int STEP_READY = 2;
+    private int STEP_PLOW = 3;
+
+    private SpriteRenderer seedSpriteRenderer;
+    private SpriteRenderer productSpriteRenderer;
+    private SpriteRenderer cropSpriteRenderer;
+
+    private Item cropItem;
+    private int step = 0;
+
+    private GameObject player;
+    private bool readyForAction;
+
+    void Start()
+    {
+        cropSpriteRenderer = GetComponent<SpriteRenderer>();
+        seedSpriteRenderer = GetComponentsInChildren<SpriteRenderer>()[2];
+        productSpriteRenderer = GetComponentsInChildren<SpriteRenderer>()[1];
+
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Item item = new Item("beet", "Food/beet", 1, Item.TYPEPFOOD, 10, 1, 5f);
+
+        if (readyForAction)
+        {
+            if (step == STEP_EMPTY)
+            {
+                if (item.type == Item.TYPEPFOOD)
+                {
+                    step = STEP_GROWS;
+                    cropItem = item;
+                    seedSpriteRenderer.sprite = Resources.Load<Sprite>("Food/seeds");
+                    StartCoroutine(grow());
+                }
+            }
+            else if (step == STEP_READY)
+            {
+                productSpriteRenderer.sprite = Resources.Load<Sprite>("Food/empty");
+                seedSpriteRenderer.sprite = Resources.Load<Sprite>("Food/extraDirt");
+            }
+        }
+    }
+
+    private IEnumerator grow()
+    {
+        yield return new WaitForSeconds(cropItem.timeToGrow);
+
+        seedSpriteRenderer.sprite = Resources.Load<Sprite>("Food/empty");
+        productSpriteRenderer.sprite = Resources.Load<Sprite>(cropItem.imgUrl);
+
+        step = STEP_READY;
+    }
+
+    private void FixedUpdate()
+    {
+        if (step != STEP_GROWS)
+        {
+            if (Vector3.Distance(this.transform.position, player.transform.position) < 0.2f)
+            {
+                readyForAction = true;
+                cropSpriteRenderer.sprite = Resources.Load<Sprite>("Food/cropSelected");
+            }
+            else
+            {
+                readyForAction = false;
+                cropSpriteRenderer.sprite = Resources.Load<Sprite>("Food/crop");
+            }
+        }
+        else
+        {
+            cropSpriteRenderer.sprite = Resources.Load<Sprite>("Food/crop");
+        } 
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿
+
+public class Item
+{
+    public static int TYPEPFOOD = 1;
+
+    public string name;
+    public string imgUrl;
+    public int type;
+    public int count;
+    public int price;
+    public int lvlWhenUnlock;
+    public float timeToGrow;
+
+    public Item(string name, string imgUrl, int count, int type, int price, int lvlWhenUnlock, float timeToGrow)
+    {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.count = count;
+        this.type = type;
+        this.price = price;
+        this.lvlWhenUnlock = lvlWhenUnlock;
+        this.timeToGrow = timeToGrow;
+    }
+}
